3542f6b6233b5f2a7ed88ee3a29450cb6627a046
Martin <martin.stankard@hotmail.com>
2021-01-29 10:19:24 -0700

    formatting

--- schema.js
+++ schema.js
@@ -5,7 +5,7 @@ const {
   GraphQLInt,
   GraphQLSchema,
   GraphQLList,
-  GraphQLBoolean
+  GraphQLBoolean,
 } = require("graphql");
 
 const jhp = "https://jsonplaceholder.typicode.com/";
@@ -17,8 +17,8 @@ const PostType = new GraphQLObjectType({
     userId: { type: GraphQLInt },
     id: { type: GraphQLInt },
     title: { type: GraphQLString },
-    body: { type: GraphQLString }
-  })
+    body: { type: GraphQLString },
+  }),
 });
 
 const CommentType = new GraphQLObjectType({
@@ -29,8 +29,8 @@ const CommentType = new GraphQLObjectType({
     id: { type: GraphQLInt },
     name: { type: GraphQLString },
     email: { type: GraphQLString },
-    body: { type: GraphQLString }
-  })
+    body: { type: GraphQLString },
+  }),
 });
 
 const AlbumType = new GraphQLObjectType({
@@ -39,8 +39,8 @@ const AlbumType = new GraphQLObjectType({
     user: userGetter,
     userId: { type: GraphQLInt },
     id: { type: GraphQLInt },
-    title: { type: GraphQLString }
-  })
+    title: { type: GraphQLString },
+  }),
 });
 
 const PhotoType = new GraphQLObjectType({
@@ -51,8 +51,8 @@ const PhotoType = new GraphQLObjectType({
     id: { type: GraphQLInt },
     title: { type: GraphQLString },
     url: { type: GraphQLString },
-    thumbnailUrl: { type: GraphQLString }
-  })
+    thumbnailUrl: { type: GraphQLString },
+  }),
 });
 
 const TodoType = new GraphQLObjectType({
@@ -62,8 +62,8 @@ const TodoType = new GraphQLObjectType({
     userId: { type: GraphQLInt },
     id: { type: GraphQLInt },
     title: { type: GraphQLString },
-    completed: { type: GraphQLBoolean }
-  })
+    completed: { type: GraphQLBoolean },
+  }),
 });
 
 const UserType = new GraphQLObjectType({
@@ -74,8 +74,8 @@ const UserType = new GraphQLObjectType({
     username: { type: GraphQLString },
     email: { type: GraphQLString },
     phone: { type: GraphQLString },
-    website: { type: GraphQLString }
-  })
+    website: { type: GraphQLString },
+  }),
 });
 
 //root query
@@ -87,134 +87,134 @@ const RootQuery = new GraphQLObjectType({
       type: PostType,
       args: {
         id: {
-          type: GraphQLInt
-        }
+          type: GraphQLInt,
+        },
       },
       resolve(parentValue, args) {
-        return axios.get(jhp + "posts/" + args.id).then(res => res.data);
-      }
+        return axios.get(jhp + "posts/" + args.id).then((res) => res.data);
+      },
     },
     posts: {
       type: new GraphQLList(PostType),
       args: {
         userId: {
-          type: GraphQLInt
-        }
+          type: GraphQLInt,
+        },
       },
       resolve(parentValue, args) {
         if (args.userId) {
           return axios
             .get(jhp + "posts?userId=" + args.userId)
-            .then(res => res.data);
+            .then((res) => res.data);
         } else {
-          return axios.get(jhp + "posts").then(res => res.data);
+          return axios.get(jhp + "posts").then((res) => res.data);
         }
-      }
+      },
     },
     comment: {
       type: CommentType,
       args: {
         id: {
-          type: GraphQLInt
-        }
+          type: GraphQLInt,
+        },
       },
       resolve(parentValue, args) {
-        return axios.get(jhp + "comments/" + args.id).then(res => res.data);
-      }
+        return axios.get(jhp + "comments/" + args.id).then((res) => res.data);
+      },
     },
     comments: {
       type: new GraphQLList(CommentType),
       args: {
         postId: {
-          type: GraphQLInt
-        }
+          type: GraphQLInt,
+        },
       },
       resolve(parentValue, args) {
         if (args.postId) {
           return axios
             .get(jhp + "comments?postId=" + args.postId)
-            .then(res => res.data);
+            .then((res) => res.data);
         } else {
-          return axios.get(jhp + "comments").then(res => res.data);
+          return axios.get(jhp + "comments").then((res) => res.data);
         }
-      }
+      },
     },
     album: {
       type: AlbumType,
       args: {
         id: {
-          type: GraphQLInt
-        }
+          type: GraphQLInt,
+        },
       },
       resolve(parentValue, args) {
-        return axios.get(jhp + "albums/" + args.id).then(res => res.data);
-      }
+        return axios.get(jhp + "albums/" + args.id).then((res) => res.data);
+      },
     },
     albums: {
       type: new GraphQLList(AlbumType),
       args: {
         userId: {
-          type: GraphQLInt
-        }
+          type: GraphQLInt,
+        },
       },
       resolve(parentValue, args) {
         if (args.userId) {
           return axios
             .get(jhp + "albums?userId=" + args.userId)
-            .then(res => res.data);
+            .then((res) => res.data);
         } else {
-          return axios.get(jhp + "albums").then(res => res.data);
+          return axios.get(jhp + "albums").then((res) => res.data);
         }
-      }
+      },
     },
     photo: {
       type: PhotoType,
       args: {
         id: {
-          type: GraphQLInt
-        }
+          type: GraphQLInt,
+        },
       },
       resolve(parentValue, args) {
-        return axios.get(jhp + "photos/" + args.id).then(res => res.data);
-      }
+        return axios.get(jhp + "photos/" + args.id).then((res) => res.data);
+      },
     },
     photos: {
       type: new GraphQLList(PhotoType),
       args: {
         albumId: {
-          type: GraphQLInt
-        }
+          type: GraphQLInt,
+        },
       },
       resolve(parentValue, args) {
         if (args.albumId) {
           return axios
             .get(jhp + "photos?albumId=" + args.albumId)
-            .then(res => res.data);
+            .then((res) => res.data);
         } else {
-          return axios.get(jhp + "photos").then(res => res.data);
+          return axios.get(jhp + "photos").then((res) => res.data);
         }
-      }
+      },
     },
     todo: {
       type: TodoType,
       args: {
         id: {
-          type: GraphQLInt
-        }
+          type: GraphQLInt,
+        },
       },
       resolve(parentValue, args) {
-        return axios.get(jhp + "todos/" + args.id).then(res => res.data);
-      }
+        return axios.get(jhp + "todos/" + args.id).then((res) => res.data);
+      },
     },
     todos: {
       type: new GraphQLList(TodoType),
       args: {
         userId: {
-          type: GraphQLInt
+          type: GraphQLInt,
         },
         completed: {
-          type: GraphQLBoolean
-        }
+          type: GraphQLBoolean,
+        },
       },
       resolve(parentValue, args) {
         let argsString = "";
@@ -234,50 +234,50 @@ const RootQuery = new GraphQLObjectType({
             "?userId=" + args.userId + "&completed=" + args.completed;
         }
         //ready to feel stupid when I think of a smarter way...
-        return axios.get(jhp + "todos" + argsString).then(res => res.data);
-      }
+        return axios.get(jhp + "todos" + argsString).then((res) => res.data);
+      },
     },
     user: {
       type: UserType,
       args: {
         id: {
-          type: GraphQLInt
-        }
+          type: GraphQLInt,
+        },
       },
       resolve(parentValue, args) {
-        return axios.get(jhp + "users/" + args.id).then(res => res.data);
-      }
+        return axios.get(jhp + "users/" + args.id).then((res) => res.data);
+      },
     },
     users: {
       type: new GraphQLList(UserType),
       resolve(parentValue, args) {
-        return axios.get(jhp + "users").then(res => res.data);
-      }
-    }
-  }
+        return axios.get(jhp + "users").then((res) => res.data);
+      },
+    },
+  },
 });
 
 const userGetter = {
   type: UserType,
   resolve(parent, args) {
-    return axios.get(jhp + "users/" + parent.userId).then(res => res.data);
-  }
+    return axios.get(jhp + "users/" + parent.userId).then((res) => res.data);
+  },
 };
 
 const postGetter = {
   type: PostType,
   resolve(parent, args) {
-    return axios.get(jhp + "posts/" + parent.postId).then(res => res.data);
-  }
+    return axios.get(jhp + "posts/" + parent.postId).then((res) => res.data);
+  },
 };
 
 const albumGetter = {
   type: AlbumType,
   resolve(parent, args) {
-    return axios.get(jhp + "albums/" + parent.albumId).then(res => res.data);
-  }
+    return axios.get(jhp + "albums/" + parent.albumId).then((res) => res.data);
+  },
 };
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
 });
