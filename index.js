e2a1ae172f0e510d695ad58d2877b8f4683c1908
Barry <barry@massiveinfinity.com>
2021-02-09 15:57:20 +0800

    Include CORS support

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const expressGraphQL = require('express-graphql');
 const schema = require('./schema.js');
 
 const app = express();
+
+app.use(cors());
 
 app.use('/graphql', expressGraphQL({
     schema:schema,
